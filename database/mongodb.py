5b4b14ef61f73c85f885ad6e28d9916ab251ac7e
Prateekj7 <prateek98saikia@gmail.com>
2023-08-07 22:39:32 +0530

    Mongo connection issue fixed

--- database/mongodb.py
+++ database/mongodb.py
@@ -3,8 +3,10 @@ from pymongo import MongoClient
 # Function to connect to MongoDB
 def connect_mongodb():
     # Replace with your MongoDB connection URL
-    MONGO_CLIENT = MongoClient("mongodb://root:password@127.0.0.1:27017")
-    return MONGO_CLIENT["dbname"]["profiles"]
+    # MONGO_CLIENT = MongoClient("mongodb://root:password$@127.0.0.1:27017")
+    MONGO_CLIENT = MongoClient()
+    return MONGO_CLIENT["dbname"]["profiles"] # This is returning Mongo client with DB - dbname
+                                                # and collection profiles
 
 
 # Function to insert profile picture into MongoDB
